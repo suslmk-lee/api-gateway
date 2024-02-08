ce8d2030f3f05a955f8bf8aa31c17795da4e6de9
mctlmk <minkyu.lee@namutech.co.kr>
2024-02-08 10:59:36 +0900

    update

--- src/test/java/com/kpaas/msa/MsaApplicationTests.java
+++ src/test/java/com/kpaas/msa/MsaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MsaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
